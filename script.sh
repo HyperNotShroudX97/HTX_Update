6d63e54380683016afb273a091544ed821f863c7
HyperNotShroudX97 <81577009+HyperNotShroudX97@users.noreply.github.com>
2021-04-20 20:13:43 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,28 +7,28 @@ echo """###You're running the USERGE-X update script.###"""
 echo " "
 
 #get username of user
-echo -n "Kakashi or Itachi or gpack repo? (k)/(i)/(g): "
+echo -n "HyperUserge-UX or Itachi or MyGpack repo? (H)/(I)/(M): "
 read u_n
 
-if [[ "${u_n}" == "k" ]]; then
+if [[ "${u_n}" == "H" ]]; then
 	rm -rf HyperUserge-UX
-	git clone https://github.com/ashwinstr/UX-itachi.git
+	git clone https://github.com/HyperNotShroudX97/HyperUserge-UX.git
 	cd HyperUserge-UX
 	git pull
-	git pull https://github.com/HyperNotShroudX97/HyperUserge-UX.git
+	git pull https://github.com/ashwinstr/UX-itachi.git
 	repo="HyperUserge-UX"
 fi
 
-if [[ "${u_n}" == "i" ]]; then
+if [[ "${u_n}" == "I" ]]; then
 	rm -rf HyperUserge-UX
 	git clone https://github.com/HyperNotShroudX97/HyperUserge-UX.git
 	cd HyperUserge-UX
 	git pull
 	git pull https://github.com/code-rgb/USERGE-X.git
-	repo="HyperUserge-UX"
+	repo="HyperUserge-UX"https://github.com/HyperNotShroudX97/HyperUserge-UX.githttps://github.com/ashwinstr/UX-itachi.git
 fi
 
-if [[ "${u_n}" == "g" ]]; then
+if [[ "${u_n}" == "M" ]]; then
         rm -rf MyGpack
         git clone https://github.com/HyperNotShroudX97/MyGpack.git
         cd MyGpack
